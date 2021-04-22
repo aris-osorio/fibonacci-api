066a85b797b99f8f5f9a31540be320f5286e8de8
aris-osorio <aris.osorio@alumnos.udg.mx>
2021-04-22 03:14:05 -0500

    fibonacci v3 (optimizacion del algoritmo de fibonacci)

--- fibonacci2/resolver/resolver.py
+++ fibonacci2/resolver/resolver.py
@@ -1,33 +1,21 @@
-def operacion(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return operacion(n-1) + operacion(n-2)
-
 class Resolver():
-    
     def fibonacci(num):
         res = 0
-        con = 0
-        lis = []
+        lis = [0,1]
         rev = []
 
         if num == 0:
-            return [0]
-
-        while(num > res):
-            res = operacion(con)
-
-            if num < res:
-                break
-
-            lis.append(res)
-            con += 1
+            return([0])
+        elif num > 1:
+            while(num > res):
+                res = lis[len(lis)-1] + lis[len(lis)-2]
+                if num < res:
+                    break
+                lis.append(res)
 
         rev = lis.copy()
         rev.pop(len(rev)-1)
         rev.reverse() 
-        return lis + rev
+        return(lis + rev)
+
 
